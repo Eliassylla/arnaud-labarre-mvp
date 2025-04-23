c2e49a726748bb8c1017488a9abb9307af530cae
Eliassylla <elssylla24@gmail.com>
2025-04-23 19:04:52 +0100

    Fix: Suppression des imports non utilisés dans login-form.tsx

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -8,7 +8,6 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { useEffect, useState } from "react"
 
 export function LoginForm({
   className,
