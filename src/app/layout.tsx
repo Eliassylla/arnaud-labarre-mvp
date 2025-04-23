6f2dd456c955edddd5be7bb1f86b5f96fa6bc365
Eliassylla <elssylla24@gmail.com>
2025-04-23 16:57:01 +0100

    Retrait temporaire de SpeedInsights pour résoudre l'erreur de build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import ScrollToTop from "@/components/ScrollToTop";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 
@@ -52,7 +51,6 @@ export default function RootLayout({
       <body className="antialiased">
         <ScrollToTop />
         {children}
-        <SpeedInsights />
       </body>
     </html>
   );
