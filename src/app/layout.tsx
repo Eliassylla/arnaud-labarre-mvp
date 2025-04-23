617783e5db4e702e95f4f9db1a97e45555d730ad
Eliassylla <elssylla24@gmail.com>
2025-04-23 16:52:17 +0100

    Ajout de Vercel Speed Insights pour analyser les performances

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import ScrollToTop from "@/components/ScrollToTop";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 
@@ -51,6 +52,7 @@ export default function RootLayout({
       <body className="antialiased">
         <ScrollToTop />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
